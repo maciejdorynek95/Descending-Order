11b12fa202769e9ceac7c371ee3a774de33100c4
maciejdorynek95 <maciejdorynek95@gmail.com>
2021-05-24 21:41:58 +0200

    zmiana

--- Program.cs
+++ Program.cs
@@ -1,38 +1,19 @@
 ﻿using System;
+using System.Linq;
 
 namespace Kata
 {
     public class Kata
     {
+       
         public static int DescendingOrder(int num)
         {
-            #region zmienneLokalne
-            var length = (int)(Math.Log10(num) + 1);
-            char[] Numbers = new char[length];
-            var i = 0;
-            #endregion
-            foreach (char ch in num.ToString())
-            {
-                Numbers[i] = ch;
-                i++;
-            }
-            Array.Sort(Numbers);
-            Array.Reverse(Numbers);
+            return int.Parse(string.Concat(num.ToString().OrderByDescending(x => x)));
 
-            //Assert
-            var oczekiwana = 66653211;
-            //Act
-            var aktualna = Kata.DescendingOrder(11352666);
-            //Assert
-            Console.WriteLine($"Sprawdzenie: {Equals(oczekiwana, aktualna)}");
-
-            return int.Parse(String.Join("", Numbers)); //łącz
-
-           
         }
         public static void Main()
         {
-
+            Console.WriteLine(DescendingOrder(6634246));
         }
     }
 }
